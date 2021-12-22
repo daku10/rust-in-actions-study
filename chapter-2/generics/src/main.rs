5f68cdb305671be6654070937ac525615c1e94e4
daku10 <daku.two@gmail.com>
2021-12-22 15:25:42 +0900

    add generics example

---
+++ chapter-2/generics/src/main.rs
@@ -0,0 +1,17 @@
+use std::{ops::Add, time::Duration};
+
+fn add<T: Add<Output = T>>(i: T, j: T) -> T {
+    i + j
+}
+
+fn main() {
+    let floats = add(1.2, 3.4);
+    let ints = add(10, 20);
+    let durations = add(
+        Duration::new(5, 0),
+        Duration::new(10, 0)
+    );
+    println!("{}", floats);
+    println!("{}", ints);
+    println!("{:?}", durations);
+}
