c4851db0762c1c3b3ea30dfbe74454c60b2c1d0b
daku10 <daku.two@gmail.com>
2021-12-22 15:34:16 +0900

    use enumerate

--- chapter-2/str-simple-pattern/src/main.rs
+++ chapter-2/str-simple-pattern/src/main.rs
@@ -7,12 +7,11 @@ It is the same with books.
 What do we seek through millions of pages?";
     // lines()が返す引用文のイテレータで、各テキスト行が反復処理される
     // 改行の解釈は実機OSの規約が使われる
-    let mut line_num: usize = 1;
-
-    for line in quote.lines() {
+   
+    for (i, line) in quote.lines().enumerate() {
+        let line_num = i + 1;
         if line.contains(search_term) {
             println!("{}: {}", line_num, line);
         }
-        line_num += 1;
     }
 }
