26d5caf7c0c7eaffdaf42f461e8dcfc552f0fc71
daku10 <daku.two@gmail.com>
2021-12-22 15:31:38 +0900

    add simple str search pattern

---
+++ chapter-2/str-simple-pattern/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let search_term = "picture";
+    let quote = "\
+Every face, every shop, bedroom window, public-house, and
+dark square is a picture feverishly turned--in search of what?
+It is the same with books.
+What do we seek through millions of pages?";
+    // lines()が返す引用文のイテレータで、各テキスト行が反復処理される
+    // 改行の解釈は実機OSの規約が使われる
+    for line in quote.lines() {
+        if line.contains(search_term) {
+            println!("{}", line);
+        }
+    }
+}
