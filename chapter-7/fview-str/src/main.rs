f19be788464273239b45dac6ae01bf4224c61bfc
daku10 <daku.two@gmail.com>
2021-12-28 18:22:44 +0900

    handling file

--- chapter-7/fview-str/src/main.rs
+++ chapter-7/fview-str/src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Read;
+use std::{env, fs::File, io::Read};
 
 const BYTES_PER_LINE: usize = 16;
 const INPUT: &'static [u8] = br#"
@@ -6,19 +6,26 @@ fn main() {
     println!("Hello, world!");
 }"#;
 
-fn main() -> std::io::Result<()> {
-    let mut buffer: Vec<u8> = vec![];
-    INPUT.read_to_end(&mut buffer);
+fn main() {
+    let arg1 = env::args().nth(1);
 
-    let mut position_in_input = 0;
-    for line in buffer.chunks(BYTES_PER_LINE) {
-        print!("[0x{:08x} ", position_in_input);
-        for byte in line {
-            print!("{:02x} ", byte);
+    let fname = arg1.expect("usage: fview FILENAME");
+
+    let mut f = File::open(&fname).expect("Unable to openfile.");
+    let mut pos = 0;
+    let mut buffer = [0; BYTES_PER_LINE];
+
+    while let Ok(_) = f.read_exact(&mut buffer) {
+        print!("[0x{:08x}] ", pos);
+        for byte in &buffer {
+            match *byte {
+                0x00 => print!(". "),
+                0xff => print!("## "),
+                _ => print!("{:02x} ", byte),
+            }
         }
-        println!();
-        position_in_input += BYTES_PER_LINE;
-    }
 
-    Ok(())
+        println!("");
+        pos += BYTES_PER_LINE;
+    }
 }
