3b1f2c57a434b0f9773c6a600da26c1bba71a008
daku10 <daku.two@gmail.com>
2022-01-03 18:04:29 +0900

    add channel example

---
+++ chapter-10/channels-intro/src/main.rs
@@ -0,0 +1,15 @@
+use std::thread;
+
+use crossbeam::{select, unbounded};
+
+fn main() {
+    let (tx, rx) = unbounded();
+
+    thread::spawn(move || {
+        tx.send(42).unwrap();
+    });
+
+    select! {
+        recv(rx) -> msg => println!("{:?}", msg)
+    }
+}
